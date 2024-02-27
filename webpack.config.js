34b95ac8eaa7343b297d65ac0bec3e03760bb413
Howl Kimura <howlkimuradev@gmail.com>
2024-02-27 15:32:22 +0300

    configure html loader for images in html

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -16,6 +17,9 @@ module.exports = {
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
+        generator: {
+          filename: 'images/[name]-[hash][ext]'
+        }
       },
       {
         test: /\.s[ac]ss$/i,
@@ -28,6 +32,17 @@ module.exports = {
           "sass-loader",
         ],
       },
+      {
+        test: /\.html$/i,
+        use: 'html-loader'
+      },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+        template: 'src/index.html',
+        filename: 'index.html',
+        inject: false
+    })
+  ],
 };
