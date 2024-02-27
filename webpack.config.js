6f599799a258713eefff7aef66d2c0b3c5aa6917
Howl Kimura <howlkimuradev@gmail.com>
2024-02-27 16:41:43 +0300

    Fix scss not compiling with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -11,26 +11,19 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
+        test: /\.(scss|css)$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
       },
+      // {
+      //   test: /\.css$/,
+      //   use: ['style-loader', 'css-loader'],
+      // },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
         generator: {
           filename: 'images/[name]-[hash][ext]'
         }
-      },
-      {
-        test: /\.s[ac]ss$/i,
-        use: [
-          // Creates `style` nodes from JS strings
-          "style-loader",
-          // Translates CSS into CommonJS
-          "css-loader",
-          // Compiles Sass to CSS
-          "sass-loader",
-        ],
       },
       {
         test: /\.html$/i,
